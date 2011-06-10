1d70d9ae4994c67a7c6df790b5aea3939862e06f
junhui.shao <junhui.shao@opi-corp.com>
2011-06-10 10:17:57 +0800

    fork from httpfox with summary

---
+++ defaults/preferences/foxmeter.js
@@ -0,0 +1,6 @@
+pref("extensions.foxmeter.StartAtBrowserStart", false);
+pref("extensions.foxmeter.AlwaysOpenDetached", false);
+pref("extensions.foxmeter.ShowFoxMeterHelperRequests", false);
+pref("extensions.foxmeter.ColorRequests", true);
+pref("extensions.foxmeter.ShowDebugTab", false);
+pref("extensions.foxmeter.ForceCaching", true);
